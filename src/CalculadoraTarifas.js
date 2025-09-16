6ef54254b541844f75568a8f33907f110ae649ba
LokoAddams <lucasalcoba74@gmail.com>
2025-09-16 06:06:05 -0400

    refactor: usar tipo de dato "Date"

--- src/CalculadoraTarifas.js
+++ src/CalculadoraTarifas.js
@@ -1,11 +1,16 @@
 class CalculadoraTarifas {
   #fecha;
   constructor(nuevaFecha) {
-    this.#fecha = new String(nuevaFecha);
+    this.#fecha = new Date(nuevaFecha);
   }
   
   obtenerFechaFormateada() {
-    return this.#fecha.toString();
+    const anio = this.#fecha.getFullYear();
+    const mes = String(this.#fecha.getMonth() + 1).padStart(2, '0'); // Los meses son de 0-11
+    const dia = String(this.#fecha.getDate()).padStart(2, '0');
+    const horas = String(this.#fecha.getHours()).padStart(2, '0');
+    const minutos = String(this.#fecha.getMinutes()).padStart(2, '0');
+    return `${anio}-${mes}-${dia} ${horas}:${minutos}`;
   }
 }
 module.exports = CalculadoraTarifas;
