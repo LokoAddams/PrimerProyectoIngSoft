d7e277238f1010a5af16ba12930db5739adf73d0
LokoAddams <lucasalcoba74@gmail.com>
2025-09-16 06:06:53 -0400

    test: formato invalido

--- src/CalculadoraTarifas.test.js
+++ src/CalculadoraTarifas.test.js
@@ -7,3 +7,11 @@ describe("Ingresar la hora de ENTRADA y que se vea en pantalla", () => {
     expect(obtenerFecha).toEqual('2025-09-10 18:00');
   })
 })
+
+describe("Formato de fecha invalido", () => {
+  it("Deberia mostrar un mensaje de error para formato de fecha invalido", () => {
+    const calculadoraTarifas = new CalculadoraTarifas('2025');
+    const obtenerFecha = calculadoraTarifas.obtenerFechaFormateada(); 
+    expect(obtenerFecha).toEqual('Formato de fecha inválido. Use YYYY-MM-DD HH:MM.');
+  })
+})
